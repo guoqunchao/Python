525ee61d3098eab520174491a8c00b5adca56377
Ink.whiteゞ <835868899@qq.com>
2020-05-25 17:24:52 +0800

    add file ...

--- Python3入门到精通/001用Python设计第一个游戏/main.py
+++ Python3入门到精通/001用Python设计第一个游戏/main.py
@@ -1,16 +1,21 @@
-print("------------------------------------------")
-a = 0
-while a <3:
-    temp = input("不妨猜一下小甲鱼现在心情想的是哪个数字: ")
-    guess = int(temp)
-    a += 1
-    if guess == 8:
-        print("卧槽，你是小甲鱼心里的蛔虫吗？！")
-        print("哼，猜中了也没有奖励！")
-        break
-    else:
-        if guess > 8:
-            print("哥，大了大了~~")
-        else:
-            print("嘿，小了小了~~")
-print("游戏结束，不玩啦(●'◡'●)")
+print("-------------------------------------")
+a = 0
+while a < 3:
+    temp = input("不妨猜一下小甲鱼现在心情想的是哪个数字: ")
+    guess = int(temp)
+    a += 1
+    if guess == 8:
+        print("卧槽，你是小甲鱼心里的蛔虫吗？")
+        print("哼，猜中了也没有奖励！")
+        break
+    else:
+        if guess > 8:
+            print("哥，大了大了")
+        else:
+            print("嘿，小了小了")
+print("游戏结束，不玩啦O(∩_∩)O！")
+
+
+
+
+
