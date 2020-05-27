080ecf9949959725df5bcf93ac30985de49f92a4
Ink.whiteゞ <835868899@qq.com>
2020-05-27 11:35:02 +0800

    update file ....

--- Python3入门到精通/001用Python设计第一个游戏/main.py
+++ Python3入门到精通/001用Python设计第一个游戏/main.py
@@ -10,10 +10,10 @@ while True:
         sys.exit(1)
     guess = int(temp)
     if guess == secret:
-        print("哇 你太厉害了！")
+        print(">> 哇 你太厉害了！")
         break
     elif guess > secret:
-        print("哥哥 大了大了！")
+        print(">> 哥哥 大了大了！")
     else:
-        print("弟弟 小了小了")
+        print(">> 弟弟 小了小了")
 print("拜拜~  游戏结束！")
