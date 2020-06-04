35b9701fc834e4d3ed35711f0b8eacbcd47b139a
Ink.whiteゞ <835868899@qq.com>
2020-06-04 10:15:40 +0800

    update file ...

--- Python3入门到精通/009格式化/main.py
+++ Python3入门到精通/009格式化/main.py
@@ -3,3 +3,10 @@ print("{0} love {1}.{2}".format("I","FishC","com"))
 
 # 关键字参数
 print("{a} love {b}.{c}".format(a="I",b="FishC",c="com"))
+
+# %s 格式化字符串
+print("%s" %("I love FishC.com"))
+
+# %d 格式化整数
+print("%d + %d = %d" %(4,5,4+5))
+
