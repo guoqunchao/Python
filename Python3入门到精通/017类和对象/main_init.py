98dd96ec67a093c1262a463318de9f6652950ce4
Ink.whiteゞ <835868899@qq.com>
2020-08-08 17:53:15 +0800

    update...

--- Python3入门到精通/017类和对象/main_init.py
+++ Python3入门到精通/017类和对象/main_init.py
@@ -1,11 +1,11 @@
 # -*- coding:utf-8 -*-
 
 class Ball():
-    def setName(self,name):
+    def __init__(self,name):
         self.name = name
+
     def kick(self):
         print("我叫%s, 该死的，谁踢我..." %self.name)
 
-a = Ball()
-a.setName("a")
+a = Ball('a')
 a.kick()
